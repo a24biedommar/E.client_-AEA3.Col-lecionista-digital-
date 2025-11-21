440d1df514f185df1d0c8d0cb6ce3a28305fe806
«a24biedommar» <a24biedommar@inspedralbes.cat~>
2025-11-21 10:38:03 +0100

    Afegir store de favorits i separar el cercador

---
+++ src/components/Search/SearchBarComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+//Definim l'event que emetrem al component pare (SearchView), aquest cas "search"
+const emit = defineEmits(["search"]);
+
+//Variable reactiva per emmagatzemar el valor de l'input (inicialment buida)
+const searchQuery = ref("");
+
+function submitSearch() {
+  // Si el input no està buit, enviem l'event "search" amb el valor de l'input
+  if (searchQuery.value !== "") {
+    emit("search", searchQuery.value);
+  }
+}
+</script>
+
+<template>
+  <div class="search-bar-container">
+    <div class="input-group">
+      <input
+        v-model="searchQuery"
+        type="number"
+        placeholder="Introdueix ID..."
+        class="form-control"
+        @keyup.enter="submitSearch"
+      />
+      <button @click="submitSearch" class="btn-search">Cercar</button>
+    </div>
+  </div>
+</template>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,17 +1,21 @@
 <script setup>
 // IMPORTACIONS NECESSARIES
 import { ref } from "vue";
-import SearchInputComponent from "../components/Search/SearchImputComponent.vue";
+import SearchBar from "../components/Search/SearchBarComponent.vue";
 import SearchCategoryComponent from "../components/Search/SearchCategoryComponent.vue";
 import ShowAllItems from "../components/ShowAllItemsComponent.vue";
 import ItemCard from "../components/ItemCardComponent.vue";
-import { getProductsByCategory } from "../services/communicationManager.js";
+import {
+  getProductsByCategory,
+  getProductByID,
+} from "../services/communicationManager.js";
 
 // VARIABLES REACTIVES PER A GESTIONAR ELS PRODUCTES DE LA CATEGORIA SELECCIONADA
 const categoryProducts = ref([]); // LLISTA D'ITEMS DE LA CATEGORIA ACTUAL
 const currentCategory = ref(""); // NOM DE LA CATEGORIA ACTUAL
 const isCategoryActive = ref(false); // INDICA SI HI HA UNA CATEGORIA ACTIVA (PER NO MOSTRAR TOTS ELS ITEMS)
 const isSearchActive = ref(false); // INDICA SI S'HA REALITZAT UNA CERCA (PER NO MOSTRAR ELS ITEMS DE LA CATEGORIA)
+const searchProducts = ref([]); // GUARDA EL RESULTAT DE LA CERCA
 
 // FUNCIO PER A CARREGAR ELS PRODUCTES DE LA CATEGORIA SELECCIONADA (REP LES DADES DEL COMPONENT FILLS (SEARCHCATEGORYCOMPONENT))
 async function handleCategoryLoad(category) {
@@ -22,11 +26,18 @@ async function handleCategoryLoad(category) {
   isSearchActive.value = false;
 }
 // FUNCIO QUE SE EXECUTA QUE S'EXECUTA QUAN EL FILL (SEARCHIMPUT) EMET 'SEARCH-DONE'
-function handleSearchActive() {
-  // Desactivamos la categoría
+async function handleSearchActive(id) {
+  // Desactivem la categoria activa
   isCategoryActive.value = false;
-  // 2. ACTIVAMOS EL MODO BÚSQUEDA
+  // Activem l'estat de cerca
   isSearchActive.value = true;
+  // Netejem els productes de cerca
+  searchProducts.value = [];
+
+  // Cridem a l afunció del servei per obtenir l'item per ID
+  const response = await getProductByID(id);
+  // Guardem el resultat a la nostra nova variable (dins d'un array)
+  searchProducts.value = [response.data];
 }
 </script>
 
@@ -39,7 +50,7 @@ function handleSearchActive() {
 
     <main class="search-main">
       <!-- INPUT DE CERCA GENERAL -->
-      <SearchInputComponent @search-done="handleSearchActive" />
+      <SearchBar @search="handleSearchActive" />
     </main>
 
     <section class="search-footer">
@@ -56,8 +67,20 @@ function handleSearchActive() {
           />
         </div>
       </div>
+
+      <div v-else-if="isSearchActive" class="search-results">
+        <h3>Resultat de la cerca:</h3>
+        <div class="items-grid-container">
+          <ItemCard
+            v-for="prod in searchProducts"
+            :key="prod.id"
+            :item="prod"
+          />
+        </div>
+      </div>
+
       <!-- MOSTRAR TOTS ELS ITEMS (ELSE) -->
-      <ShowAllItems v-else-if="!isSearchActive" />
+      <ShowAllItems v-else />
     </section>
   </div>
 </template>
